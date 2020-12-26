03c66c47f4ee5406c6ba73c4e2c113cf82427f76
nguyenleanhkhoa <anhkhoalenguyen28@gmail.com>
2020-12-26 15:13:25 +0700

    Commit delete google font in pubspec

--- lib/screens/signin.screen.dart
+++ lib/screens/signin.screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:wru_fe/cubit/signin_cubit.dart';
 import 'package:wru_fe/dto/signin.dto.dart';
 import 'package:wru_fe/screens/home.screen.dart';
